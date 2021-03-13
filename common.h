cd8e97d655bc37123e047a382b74e20579833be3
MasFlam <masflam@masflam.com>
2021-03-13 23:37:07 +0100

    Add a bunch of implementation code in We need to figure out cross-platform TCP

---
+++ common.h
@@ -0,0 +1,211 @@
+#include <stdint.h>
+
+#define STAT_OK 0
+#define STAT_FU 1
+
+struct Chat {
+	uint16_t id;
+	uint8_t isopen;
+	uint16_t namelen;
+	char name[];
+};
+
+struct User {
+	uint16_t id;
+	uint16_t namelen;
+	char name[];
+};
+
+
+#define OP_HELLO 1
+
+struct Hello {
+	uint8_t op;
+	uint16_t namelen;
+	char name[];
+};
+
+#define OP_HELLO_RESP 2
+
+struct HelloResp {
+	uint8_t op;
+	uint8_t status;
+};
+
+#define OP_FORCED_DISCONNECT 3
+
+struct ForcedDisconnect {
+	uint8_t op;
+	uint16_t reasonlen;
+	char reason[];
+};
+
+#define OP_GET_USER_LIST 4
+
+struct GetUserList {
+	uint8_t op;
+};
+
+#define OP_GET_USER_LIST_RESP 5
+
+struct GetUserListResp {
+	uint8_t op;
+	uint8_t status;
+	uint16_t userslen;
+	struct User users[];
+};
+
+#define OP_GET_CHAT_LIST 6
+
+struct GetChatList {
+	uint8_t op;
+};
+
+#define OP_GET_CHAT_LIST_RESP 7
+
+struct GetChatListResp {
+	uint8_t op;
+	uint8_t status;
+	uint16_t chatslen;
+	struct Chat chats[];
+};
+
+#define OP_CREATE_OPEN_CHAT 8
+
+struct CreateOpenChat {
+	uint8_t op;
+	uint16_t namelen;
+	char name[];
+};
+
+#define OP_CREATE_OPEN_CHAT_RESP 9
+
+#define STAT_CREATE_OPEN_CHAT_TOO_MANY 2
+#define STAT_CREATE_OPEN_CHAT_BAD_NAME 3
+
+struct CreateOpenChatResp {
+	uint8_t op;
+	uint8_t status;
+	uint16_t id;
+};
+
+#define OP_CREATE_PASSWORD_CHAT 10
+
+struct CreatePasswordChat {
+	uint8_t op;
+	uint16_t namelen;
+	uint16_t passlen;
+	char name_pass[];
+};
+
+#define OP_CREATE_PASSWORD_CHAT_RESP 11
+
+#define STAT_CREATE_PASSWORD_CHAT_TOO_MANY 2
+#define STAT_CREATE_PASSWORD_CHAT_BAD_NAME 3
+
+struct CreatePasswordChatResp {
+	uint8_t op;
+	uint8_t status;
+	uint16_t id;
+};
+
+#define OP_JOIN_OPEN_CHAT 12
+
+struct JoinOpenChat {
+	uint8_t op;
+	uint16_t id;
+};
+
+#define OP_JOIN_OPEN_CHAT_RESP 13
+
+#define STAT_JOIN_OPEN_CHAT_BAD_CHAT       2
+#define STAT_JOIN_OPEN_CHAT_ALREADY_JOINED 3
+#define STAT_JOIN_OPEN_CHAT_NOT_OPEN       4
+
+struct JoinOpenChatResp {
+	uint8_t op;
+	uint8_t status;
+};
+
+#define OP_JOIN_PASSWORD_CHAT 14
+
+struct JoinPasswordChat {
+	uint8_t op;
+	uint16_t id;
+	uint16_t passlen;
+	char pass[];
+};
+
+#define OP_JOIN_PASSWORD_CHAT_RESP 15
+
+#define STAT_JOIN_PASSWORD_CHAT_BAD_CHAT       2
+#define STAT_JOIN_PASSWORD_CHAT_ALREADY_JOINED 3
+#define STAT_JOIN_PASSWORD_CHAT_NOT_PASSWORD   4
+#define STAT_JOIN_PASSWORD_CHAT_BAD_PASSWORD   5
+
+struct JoinPasswordChatResp {
+	uint8_t op;
+	uint8_t status;
+};
+
+#define OP_USER_JOINED_CHAT 16
+
+struct UserJoinedChat {
+	uint8_t op;
+	uint16_t chatid;
+	uint16_t userid;
+};
+
+#define OP_SEND_MESSAGE 17
+
+struct SendMessage {
+	uint8_t op;
+	uint16_t chatid;
+	uint16_t msglen;
+	char msg[];
+};
+
+#define OP_SEND_MESSAGE_RESP 18
+
+#define STAT_SEND_MESSAGE_BAD_CHAT    2
+#define STAT_SEND_MESSAGE_NOT_IN_CHAT 3
+
+struct SendMessageResp {
+	uint8_t op;
+	uint8_t status;
+};
+
+#define OP_RECEIVE_MESSAGE 19
+
+struct ReceiveMessage {
+	uint8_t op;
+	uint16_t chatid;
+	uint16_t userid;
+	uint16_t msglen;
+	char msg[];
+};
+
+#define OP_SEND_DIRECT 20
+
+struct SendDirect {
+	uint8_t op;
+	uint16_t userid;
+	uint16_t msglen;
+	char msg[];
+};
+
+#define OP_SEND_DIRECT_RESP 21
+
+struct SendDirectResp {
+	uint8_t op;
+	uint8_t status;
+};
+
+#define OP_RECEIVE_DIRECT 22
+
+struct ReceiveDirect {
+	uint8_t op;
+	uint16_t userid;
+	uint16_t msglen;
+	char msg[];
+};
--- server/server.c
+++ server/server.c
@@ -1,33 +1,244 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-#include "server.h"
+#include "../common.h"
 
 
-void change_title(struct chat *chat, const char *title);
-void send_message(struct chat *chat, const struct message *msg);
+#define MAX_CHATS 256
+#define MAX_USERS 256
 
 
-void
-change_title(struct chat *chat, const char *title)
+struct chat {
+	uint16_t namelen;
+	char *name;
+	uint16_t passlen; // 0 if no pass
+	char *pass;       // NULL if no pass
+	uint16_t users_count;
+	uint8_t users[MAX_USERS/8];
+};
+
+struct user {
+	uint16_t namelen;
+	char *name;
+};
+
+
+uint16_t g_chats_count, g_users_count;
+uint8_t g_chats_ids[MAX_CHATS/8], g_users_ids[MAX_USERS/8];
+struct chat g_chats[MAX_CHATS];
+struct user g_users[MAX_USERS];
+
+
+static inline int bitset_get(uint8_t *bitset, size_t ind);
+static inline void bitset_set(uint8_t *bitset, size_t ind);
+static void create_open_chat(uint16_t userid, const struct CreateOpenChat *req, struct CreateOpenChatResp *restrict resp);
+static void create_password_chat(uint16_t userid, const struct CreatePasswordChat *req, struct CreatePasswordChatResp *restrict resp);
+#if !defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 200809L
+static inline char *strdup(const char *str);
+static char *strndup(const char *str, size_t len);
+#endif // _POSIX_C_SOURCE
+static void join_open_chat(uint16_t userid, const struct JoinOpenChat *req, struct JoinOpenChatResp *restrict resp);
+static void join_password_chat(uint16_t userid, const struct JoinPasswordChat *req, struct JoinPasswordChatResp *restrict resp);
+static uint16_t gen_chatid(bool set);
+static void receive_message(uint16_t userid, const struct SendMessage *req, struct ReceiveMessage *restrict resp);
+static void send_message(uint16_t userid, const struct SendMessage *req, struct SendMessageResp *restrict resp);
+
+
+int
+bitset_get(uint8_t *bitset, size_t ind)
 {
-	strncpy(chat->title, title, MAX_TITLE_LEN);
+	return bitset[ind / 8] & (1 << ind % 8);
 }
 
 void
-send_message(struct chat *chat, const struct message *msg)
+bitset_set(uint8_t *bitset, size_t ind)
 {
-	struct message *message = &chat->messages[chat->next_msg_ind];
-	
-	strncpy(message->text, msg->text, MAX_MESSAGE_LEN);
-	strncpy(message->author, msg->author, MAX_NAME_LEN);
-	
-	++chat->next_msg_ind;
-	chat->next_msg_ind %= MAX_MESSAGES;
+	bitset[ind / 8] |= 1 << ind % 8;
 }
 
+void
+create_open_chat(uint16_t userid, const struct CreateOpenChat *req, struct CreateOpenChatResp *restrict resp)
+{
+	uint16_t chatid;
+	
+	if ((chatid = gen_chatid(true)) == 0) {
+		resp->status = STAT_CREATE_OPEN_CHAT_TOO_MANY;
+		return;
+	}
+	
+	g_chats[chatid-1].namelen = req->namelen;
+	g_chats[chatid-1].name = strndup(req->name, req->namelen);
+	g_chats[chatid-1].passlen = 0;
+	g_chats[chatid-1].pass = NULL;
+	g_chats[chatid-1].users_count = 0;
+	memset(g_chats[chatid-1].users, 0, MAX_USERS/8);
+	
+	++g_chats_count;
+	
+	resp->status = STAT_OK;
+	resp->id = chatid;
+}
+
+void
+create_password_chat(uint16_t userid, const struct CreatePasswordChat *req, struct CreatePasswordChatResp *restrict resp)
+{
+	uint16_t chatid;
+	
+	if ((chatid = gen_chatid(true)) == 0) {
+		resp->status = STAT_CREATE_PASSWORD_CHAT_TOO_MANY;
+		return;
+	}
+	
+	g_chats[chatid-1].namelen = req->namelen;
+	g_chats[chatid-1].passlen = req->passlen;
+	g_chats[chatid-1].name = strndup(req->name_pass, req->namelen);
+	g_chats[chatid-1].pass = strndup(req->name_pass + req->namelen, req->passlen);
+	g_chats[chatid-1].users_count = 0;
+	memset(g_chats[chatid-1].users, 0, MAX_USERS/8);
+	
+	++g_chats_count;
+	
+	resp->status = STAT_OK;
+	resp->id = chatid;
+}
+
+#if !defined(_POSIX_C_SOURCE) || _POSIX_C_SOURCE < 200809L
+
+char *
+strdup(const char *str)
+{
+	return strndup(str, strlen(str));
+}
+
+char *
+strndup(const char *str, size_t len)
+{
+	char *s2 = malloc(len + 1);
+	memcpy(s2, str, len);
+	return s2;
+}
+
+#endif // _POSIX_C_SOURCE
+
+void
+join_open_chat(uint16_t userid, const struct JoinOpenChat *req, struct JoinOpenChatResp *restrict resp)
+{
+	struct chat *chat;
+	
+	if (req->id > MAX_CHATS || !bitset_get(g_chats_ids, req->id)) {
+		resp->status = STAT_JOIN_OPEN_CHAT_BAD_CHAT;
+		return;
+	}
+	
+	chat = g_chats + req->id - 1;
+	
+	if (bitset_get(chat->users, userid - 1)) {
+		resp->status = STAT_JOIN_OPEN_CHAT_ALREADY_JOINED;
+		return;
+	}
+	
+	if (chat->pass) {
+		resp->status = STAT_JOIN_OPEN_CHAT_NOT_OPEN;
+		return;
+	}
+	
+	++chat->users_count;
+	bitset_set(chat->users, userid - 1);
+	
+	resp->status = STAT_OK;
+}
+
+void
+join_password_chat(uint16_t userid, const struct JoinPasswordChat *req, struct JoinPasswordChatResp *restrict resp)
+{
+	struct chat *chat;
+	
+	if (req->id > MAX_CHATS || !bitset_get(g_chats_ids, req->id)) {
+		resp->status = STAT_JOIN_PASSWORD_CHAT_BAD_CHAT;
+		return;
+	}
+	
+	chat = g_chats + req->id - 1;
+	
+	if (bitset_get(chat->users, userid - 1)) {
+		resp->status = STAT_JOIN_PASSWORD_CHAT_ALREADY_JOINED;
+		return;
+	}
+	
+	if (!chat->pass) {
+		resp->status = STAT_JOIN_PASSWORD_CHAT_NOT_PASSWORD;
+		return;
+	}
+	
+	if (
+		chat->passlen != req->passlen ||
+		strncmp(chat->pass, req->pass, req->passlen) != 0
+	) {
+		resp->status = STAT_JOIN_PASSWORD_CHAT_BAD_PASSWORD;
+		return;
+	}
+	
+	++chat->users_count;
+	bitset_set(chat->users, userid - 1);
+	
+	resp->status = STAT_OK;
+}
+
+uint16_t
+gen_chatid(bool set)
+{
+	for (int i = 0; i < MAX_CHATS/8; ++i) {
+		for (int off = 7; off >= 0; --off) {
+			if (!(g_chats_ids[i] & (1 << off))) {
+				int ind = 8*i + off;
+				if (set) {
+					g_chats_ids[i] |= 1 << off;
+				}
+				return ind + 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void
+receive_message(uint16_t userid, const struct SendMessage *req, struct ReceiveMessage *restrict resp)
+{
+	// By now, we've already validated chatid in send_message()
+	resp->userid = userid;
+	resp->chatid = req->chatid;
+	resp->msglen = req->msglen;
+	memcpy(resp->msg, req->msg, req->msglen);
+}
+
+void
+send_message(uint16_t userid, const struct SendMessage *req, struct SendMessageResp *restrict resp)
+{
+	struct chat *chat;
+	
+	if (req->chatid > MAX_CHATS || !bitset_get(g_chats_ids, req->chatid)) {
+		resp->status = STAT_SEND_MESSAGE_BAD_CHAT;
+		return;
+	}
+	
+	chat = g_chats + req->chatid - 1;
+	
+	if (!bitset_get(chat->users, userid - 1)) {
+		resp->status = STAT_SEND_MESSAGE_NOT_IN_CHAT;
+		return;
+	}
+	
+	resp->status = STAT_OK;
+}
 
 int
 main(int argc, char **argv)
 {
-	server(8042);
+	
+	
+	return 0;
 }
