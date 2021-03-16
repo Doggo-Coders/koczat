3bed10b48028702c0a89f00b4bd2046ab3344d6f
Tomasz "TFKls" Kulis <tfk@tfkls.dev>
2021-03-16 11:10:45 +0000

    Added some stuff that is useless cause idk what tf to do :((((

--- KoczatClient/src/KoczatClient.jl
+++ KoczatClient/src/KoczatClient.jl
@@ -2,28 +2,178 @@ module KoczatClient
 
 using Gtk, Sockets
 
+struct Chat
+	id:: Cushort
+	isopen:: Cbool
+	namelen:: Cushort
+	name:: Array{UInt8}
+end
+
+struct User
+	id:: Cushort
+	isopen:: Cbool
+	namelen:: Cushort
+	name:: Array{UInt8}
+end
+
+struct Hello 
+	op:: UInt8
+	namelen:: UInt16
+	name:: Ptr{UInt8}
+end
+
+struct HelloResp 
+	op:: UInt8
+	status:: UInt16
+end
+
+struct ForcedDisconnect 
+	op:: UInt8
+	reasonlen:: UInt16
+	reason:: Ptr{UInt8}
+end
+
+struct GetUserList 
+	op:: UInt8
+end
+
+struct GetUserListResp 
+	op:: UInt8
+	status:: UInt8
+	userslen:: UInt16
+	users:: Ptr{User}
+end
+
+struct GetChatList 
+	op:: UInt8
+end
+
+struct GetChatListResp 
+	op:: UInt8
+	status:: UInt8
+	chatslen:: UInt16
+	chats:: Ptr{Chat}
+end
+
+struct CreateOpenChat 
+	op:: UInt8
+	namelen:: UInt16
+	name:: Ptr{UInt8}
+end
+
+struct CreateOpenChatResp 
+	op:: UInt8
+	status:: UInt8
+	id:: UInt16
+end
+
+struct CreatePasswordChat 
+	op:: UInt8
+	namelen:: UInt16
+	passlen:: UInt16
+	name_pass:: Ptr{UInt8}
+end
+
+struct CreatePasswordChatResp 
+	op:: UInt8
+	status:: UInt8
+	id:: UInt16
+end
+
+struct JoinOpenChat 
+	op:: UInt8
+	id:: UInt16
+end
+
+struct JoinOpenChatResp 
+	op:: UInt8
+	status:: UInt8
+end
+
+struct JoinPasswordChat 
+	op:: UInt8
+	id:: UInt16
+	passlen:: UInt16
+	pass:: Ptr{UInt8}
+end
+
+struct JoinPasswordChatResp 
+	op:: UInt8
+	status:: UInt8
+end
+
+struct UserJoinedChat 
+	op:: UInt8
+	chatid:: UInt16
+	userid:: UInt16
+end
+
+struct SendMessage 
+	op:: UInt8
+	chatid:: UInt16
+	msglen:: UInt16
+	msg:: Ptr{UInt8}
+end
+
+struct SendMessageResp 
+	op:: UInt8
+	status:: UInt8
+end
+    
+struct ReceiveMessage 
+	op:: UInt8
+	chatid:: UInt16
+	userid:: UInt16
+	msglen:: UInt16
+	msg:: Ptr{UInt8}
+end
+
+struct SendDirect 
+	op:: UInt8
+	userid:: UInt16
+	msglen:: UInt16
+	msg:: Ptr{UInt8}
+end
+
+struct SendDirectResp 
+	op:: UInt8
+	status:: UInt8
+end
+
+struct ReceiveDirect 
+	op:: UInt8
+	userid:: UInt16
+	msglen:: UInt16
+	msg:: Ptr{UInt8}
+end
+
+
+
 const glade_xml = read(joinpath(@__DIR__, "../chatapp.glade"), String)
 
 julia_main() = (main(); Cint(0))
 
 function main(args = ARGS)
+  align_packmax(1)
 	global gtkbuilder = GtkBuilder(buffer = glade_xml)
 	global window = gtkbuilder["application"]
 	
 	signal_connect(on_connect_button_clicked, gtkbuilder["connect_button"], :clicked)
-	
+
+  println("About to show window")
 	showall(window)
 end
 
 function on_connect_button_clicked(btn)
-	println("[klik klik]")
+	println("[INFO] Connecting...")    
 	ip = parse(IPAddr, get_gtk_property(gtkbuilder["ip_entry"], :text, String))
 	port = parse(Int, get_gtk_property(gtkbuilder["port_entry"], :text, String))
 	username = get_gtk_property(gtkbuilder["username_entry"], :text, String)
 	
-	println("[] $ip:$port $username")
+	println("[INFO] $ip:$port $username")
 	
 	global conn = connect(ip, port)
+  len = uint16  
 	write(conn, b"\x01\x00\x06abcdef")
 	bytes = readavailable(conn)
 	
